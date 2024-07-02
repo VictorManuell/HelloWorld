4321583da75424b5d3709ee4c34435ff22a4a739
VictorManuell <85646663+VictorManuell@users.noreply.github.com>
2024-07-01 20:21:07 -0600

    Agregamos el @CrossOrigin para poder interacuar con la app de react java-full-stack-helloworld-app

--- src/main/java/com/upa/codigoRupestre/HelloWorld/HelloWorldAPI.java
+++ src/main/java/com/upa/codigoRupestre/HelloWorld/HelloWorldAPI.java
@@ -3,10 +3,12 @@ package com.upa.codigoRupestre.HelloWorld;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000/")  //Para los cors, colocamos la url del navegador de la app en react
 @RestController
 public class HelloWorldAPI {
 	
